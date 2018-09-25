08c4f518c9dbec745cc866867db4ebb6139a29e8
zected <41475907+zected@users.noreply.github.com>
2018-09-25 10:07:02 +0700

    #2 update
    Map

--- js/animate.js
+++ js/animate.js
@@ -13,7 +13,7 @@ window.sr = ScrollReveal({
 
 /// scroll
 $('.go-scroll').on('click', function() {  
-    $('html, body').animate({scrollTop: $(this.hash).offset().top - 1}, 1200);
+    $('html, body').animate({scrollTop: $(this.hash).offset().top - 0}, 1200);
     return false;
 });
 
@@ -25,9 +25,15 @@ sr.reveal('#article',{
     opacity: 0,
 });
 
+sr.reveal('#about',{
+    duration:1000,
+    delay:1000,
+    opacity: 0,
+});
+
 sr.reveal('#main-fitur, .whatwedo',{
-    duration:400,
-    delay:400,
+    duration:1000,
+    delay:1000,
     opacity: 0,
 });
 
